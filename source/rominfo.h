5606e49d762f2104c96ae7ae6b66e27a5ac21f48
Christian Radke <c.radke@posteo.de>
2023-08-07 20:07:50 +0200

    Reformat rominfo.h

--- source/rominfo.h
+++ source/rominfo.h
@@ -1,8 +1,15 @@
 #ifndef __ROM_INFO_H
+#define __ROM_INFO_H
 
-#define GAME_TITLE	"Change me"
-#define MAKER_CODE	"VB"
-#define GAME_CODE	"LB"
-#define ROM_VERSION	1
+
+// game title (20 chars) 	 ####################
+#define __GAME_TITLE		"GAME TITLE          "
+// maker code (2 chars)		 ##
+#define __MAKER_CODE		"VB"
+// game code (4 chars)		 ####
+#define __GAME_CODE			"LGVB"
+// revision (1.x)			 #
+#define __ROM_VERSION		 0
+
 
 #endif
