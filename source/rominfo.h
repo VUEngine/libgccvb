430cf35aefb9c4b7302aa85fba56f639a842c6c9
Christian Radke <c.radke@posteo.de>
2023-08-07 20:12:49 +0200

    Fix rominfo.h

--- source/rominfo.h
+++ source/rominfo.h
@@ -3,13 +3,13 @@
 
 
 // game title (20 chars) 	 ####################
-#define __GAME_TITLE		"GAME TITLE          "
+#define GAME_TITLE			"GAME TITLE          "
 // maker code (2 chars)		 ##
-#define __MAKER_CODE		"VB"
+#define MAKER_CODE			"VB"
 // game code (4 chars)		 ####
-#define __GAME_CODE			"LGVB"
+#define GAME_CODE			"LGVB"
 // revision (1.x)			 #
-#define __ROM_VERSION		 0
+#define ROM_VERSION			 0
 
 
 #endif
